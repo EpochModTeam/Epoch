94e45f94d1eb6b25b7876c3ed822637f4ca8c7e1
vbawol <vbawol@veteranbastards.com>
2015-07-21 16:46:58 -0500

    install steamcmd

---
+++ Tools/Install/InstallEpoch.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+SET STEAMUN=admin@youremail.net
+SET STEAMPW=chamgeme
+SET ARMASVRPATH=C:\Arma3Server
+SET ARMAAPPID=107410
+:: Workshop IDs: EpochExperimental = 455221958, Normal Epoch = 421839251
+SET ARMAWORKSHOPID=455221958
+:: remove mod folder
+rd /s/q "%ARMASVRPATH%\@Epoch"
+:: update workshop mod
+START "" /WAIT steamcmd.exe +login %STEAMUN% %STEAMPW% +workshop_download_item %ARMAAPPID% %ARMAWORKSHOPID% +quit
+:: copy files back to server 
+xcopy /s/e/y/q/i "steamapps\workshop\content\%ARMAAPPID%\%ARMAWORKSHOPID%" "%ARMASVRPATH%\@Epoch"
