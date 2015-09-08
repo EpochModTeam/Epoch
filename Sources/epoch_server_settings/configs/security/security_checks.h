c204672730487588c6b1dcdd17b839dc550e0419
vbawol <vbawol@veteranbastards.com>
2015-09-08 13:42:19 -0500

    persist queue for bases and remoteExec testing

--- Sources/epoch_server_settings/configs/security/security_checks.h
+++ Sources/epoch_server_settings/configs/security/security_checks.h
@@ -63,7 +63,7 @@ class CfgSecConf
     class addEventHandler {
         checks[] = {"Fired","InventoryClosed","InventoryOpened","Killed","HandleDamage","HandleHeal","Dammaged","Hit","HitPart"};
         Fired           = "_this call EPOCH_fnc_playerFired;";
-        InventoryClosed = "if !(EPOCH_arr_interactedObjs isEqualTo[]) then { EPOCH_S_S_VEHICLES = EPOCH_arr_interactedObjs; publicVariableServer 'EPOCH_S_S_VEHICLES'; EPOCH_arr_interactedObjs = [];};";
+		InventoryClosed = "if !(EPOCH_arr_interactedObjs isEqualTo[]) then {EPOCH_arr_interactedObjs remoteExec['EPOCH_server_save_vehicles', 2]; EPOCH_arr_interactedObjs = [];};";
         InventoryOpened = "_this spawn EPOCH_initUI; (locked (_this select 1) in [2, 3])";
         Killed          = "_this call EPOCH_fnc_playerDeath;";
         HandleDamage    = "";
