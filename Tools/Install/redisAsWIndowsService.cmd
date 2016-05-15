c591fc23833277d7cb293b849b1cc7fa8e51eaf3
Andrew Gregory <andrew@dragondesign.co.uk>
2016-05-15 16:03:27 +0100

    Create redisAsWIndowsService.cmd
    Installs reDis as a windows service.

---
+++ Tools/Install/redisAsWIndowsService.cmd
@@ -0,0 +1,2 @@
+cls
+sc create EpochRedis binpath= "C:\arma3server\DB\redis-server.exe --service-run \"C:\arma3server\DB\redis.conf\"" type= own type= interact start= auto displayname= "Epoch DB Service"
