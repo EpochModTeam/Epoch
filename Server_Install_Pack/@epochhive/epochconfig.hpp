1db988095e276c3e72c72a82f03d4cfabf071077
vbawol <vbawol@veteranbastards.com>
2016-12-29 12:18:09 -0600

    booleans in configs
    - change bools in config files to string or a number.
    - EPOCH_fnc_returnConfigEntry(V2) now also supports 0/1 via config when
    default input is a bool.
    Note: this commit may address issue with Bad conversion: bool rpt spam.
    
    It is suggested to use the following format for storing booleans in
    config:
    1 = true
    "true"=true
    0 = false
    "false"=false

--- Server_Install_Pack/@epochhive/epochconfig.hpp
+++ Server_Install_Pack/@epochhive/epochconfig.hpp
@@ -1,4 +1,4 @@
-serverRestart         = false; // true sends #shutdown command to server after the time specified in ForceRestartTime
+serverRestart         = "false"; // true sends #shutdown command to server after the time specified in ForceRestartTime
 forceRestartTime      = 14400; // 4 hour restarts
 
 StorageSlotsLimit = 1500; // Max storage slots allowed. Warning! Higher the number lower performance.
@@ -28,7 +28,7 @@ antagonistChanceLoot = 0.09; //9% chance when player click "SEARCH" on a loot ob
 cloneCost = 100; // debt incurred on player death
 
 // vehicles - Max vehicle slots is calculated from per vehicle limits below. Warning! Higher the number lower the performance.
-simulationHandlerOld = false; // When enabled this feature disables simulation on vehicles that are not nea players. Can help improve client fps at the cost of server fps.
+simulationHandlerOld = "false"; // When enabled this feature disables simulation on vehicles that are not nea players. Can help improve client fps at the cost of server fps.
 vehicleLockTime = 1800; // Controls how many seconds it takes to allow another person/group to unlock vehicle.
 removevehweapons[] = {			// remove these Weapons from spawned Vehicles
 	"missiles_DAR","gatling_30mm","gatling_20mm","missiles_DAGR","cannon_30mm_Plane_CAS_02_F","Missile_AA_03_Plane_CAS_02_F","Missile_AGM_01_Plane_CAS_02_F","Rocket_03_HE_Plane_CAS_02_F",
@@ -41,7 +41,7 @@ removevehmagazinesturret[] = {	// Remove these Magazines from the given Turret f
 };
 
 // BaseBuilding
-UseIndestructible = false;			// Enable / Disable Indestructible BaseObjects
+UseIndestructible = "false";			// Enable / Disable Indestructible BaseObjects
 IndestructibleBaseObjects[] =	{	// Can be Names or Classes
 									"Constructions_static_F",
 									"Buildable_Storage",
@@ -60,15 +60,15 @@ ExceptedBaseObjects[] = 		{	// Not Indestructible, also if in a Class of Indestr
 taxRate = 0.1; // controls the price increase for purchases
 starterTraderItems[] = { { "ItemSodaBurst", "meatballs_epoch", "MortarBucket", "CinderBlocks", "VehicleRepair", "CircuitParts", "ItemCorrugated", "PartPlankPack", "ItemRock", "ItemRope", "ItemStick" }, { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 } }; // Starter Items for fresh spawned trader first array is classnames second is quantity.
 NPCSlotsLimit = 20; // Max number of traders static or dynamic. Warning! Higher the number lower performance.
-forceStaticTraders = true; // disables traders moving from work to home
+forceStaticTraders = "true"; // disables traders moving from work to home
 
 // Markers
-showEarthQuakes = true; // show mineral viens caused by earthquakes
-showShippingContainers = true; // Show location of events based loots (plants, shipping container, Carnival)
-SHOW_TRADERS = true; // Show locations of traders
-SHOW_JAMMERS = false; // Shows location of base jammers
-SHOW_BOATLOOT = true; // Shows the location of shipwreck loot
-DEBUG_VEH = false; // DEBUG ONLY used to debug spawing of vehicles
+showEarthQuakes = "true"; // show mineral viens caused by earthquakes
+showShippingContainers = "true"; // Show location of events based loots (plants, shipping container, Carnival)
+SHOW_TRADERS = "true"; // Show locations of traders
+SHOW_JAMMERS = "false"; // Shows location of base jammers
+SHOW_BOATLOOT = "true"; // Shows the location of shipwreck loot
+DEBUG_VEH = "false"; // DEBUG ONLY used to debug spawing of vehicles
 
 // Hive Related
 expiresBuilding = "604800";  // expiration date in seconds for buildings
@@ -76,12 +76,12 @@ expiresPlayer = "2592000";  // expiration date in seconds for players
 expiresBank = "7776000";  // expiration date in seconds for players bank
 expiresVehicle = "604800";  // expiration date in seconds for vehicles
 expiresAIdata = "604800";  // expiration date in seconds for NPC Trader inventory
-hiveAsync = true; // true = asynchronous hive calls (non blocking), false = synchronous hive calls (blocking)
+hiveAsync = "true"; // true = asynchronous hive calls (non blocking), false = synchronous hive calls (blocking)
 
 // Admin Features
-hiveAdminCmdExec = false; // true = enables extra (To Be Released) feature to allow execution of code via hive.
-hiveAdminSavePlayerList = true; // true = enables saving of playerUID array to hive value PLAYERS:#InstanceID.
+hiveAdminCmdExec = "false"; // true = enables extra (To Be Released) feature to allow execution of code via hive.
+hiveAdminSavePlayerList = "true"; // true = enables saving of playerUID array to hive value PLAYERS:#InstanceID.
 hiveAdminCmdTime = 5; // how many seconds between each command queue call.
 
 // DEBUG
-useOldLoadVehicles = false;
+useOldLoadVehicles = "false";
