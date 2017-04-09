e3a94eaeb81ae77f7027a3ff1d018ad67043e1d2
vbawol <vbawol@veteranbastards.com>
2017-04-09 17:08:06 -0500

    0.5 b751

--- Tools/Build/ClientReleaseTest.cmd
+++ Tools/Build/ClientReleaseTest.cmd
@@ -1,9 +1,11 @@
 ::@ECHO OFF
-
-SET WORKSHOPPATH="G:\SteamLibrary\steamapps\common\Arma 3\@epoch"
+SET TOOLSPATH=F:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools
+SET WORKSHOPPATH=G:\SteamLibrary\steamapps\common\Arma 3\@epoch
+SET CHANGELOG="See Changelogs: https://github.com/EpochModTeam/Epoch/tree/experimental/Changelogs"
 
 :: Workshop IDs:
 :: EpochExperimental = 455221958
 :: Normal Epoch = 421839251
+SET WORKSHOPID=455221958
 
-START "" /WAIT "F:\Program Files (x86)\Steam\steamapps\common\Arma 3 Tools\Publisher\PublisherCmd.exe" update /id:455221958 /changeNote:"See Changelogs: https://github.com/EpochModTeam/Epoch/tree/experimental/Changelogs" /path:%WORKSHOPPATH%
+START "" /WAIT "%TOOLSPATH%\Publisher\PublisherCmd.exe" update /id:%WORKSHOPID% /changeNote:%CHANGELOG% /path:"%WORKSHOPPATH%"
