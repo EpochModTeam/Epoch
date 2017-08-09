9764cb9f43e99e1e438cc30c727c660913574a3b
vbawol <vbawol@veteranbastards.com>
2017-08-09 11:58:40 -0500

    experimental install scripts both server and client

---
+++ Tools/Install/InstallEpochExperimentalClient.cmd
@@ -0,0 +1,16 @@
+@ECHO OFF
+SET STEAMUN=admin@youremail.net
+SET STEAMPW=changeme
+SET ARMASVRPATH=C:\Arma3Server
+SET ARMAAPPID=107410
+SET MODFOLDER=@Epoch
+:: Workshop IDs: EpochExperimental = 455221958 Normal Epoch = 421839251
+SET ARMAWORKSHOPID=455221958
+:: remove old folder
+rd /s/q "%ARMASVRPATH%\%MODFOLDER%"
+:: update workshop mod
+START "" /WAIT steamcmd.exe +login %STEAMUN% %STEAMPW% +workshop_download_item %ARMAAPPID% %ARMAWORKSHOPID% validate +quit
+:: copy files back to server
+xcopy /s/e/y/q/i "steamapps\workshop\content\%ARMAAPPID%\%ARMAWORKSHOPID%" "%ARMASVRPATH%\%MODFOLDER%"
+del "%ARMASVRPATH%\Keys\epoch*.bikey"
+xcopy /s/e/y/q/i "%ARMASVRPATH%\%MODFOLDER%\Keys" "%ARMASVRPATH%\Keys"
---
+++ Tools/Install/InstallEpochExperimentalServer.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET STEAMUN=admin@youremail.net
+SET STEAMPW=changeme
+SET ARMASVRPATH=C:\Arma3Server
+SET ARMAAPPID=107410
+SET ARMAWORKSHOPID=558243173
+:: update workshop mod
+START "" /WAIT steamcmd.exe +login %STEAMUN% %STEAMPW% +workshop_download_item %ARMAAPPID% %ARMAWORKSHOPID% validate +quit
+:: copy files back to server
+xcopy /s/e/y/q/i "steamapps\workshop\content\%ARMAAPPID%\%ARMAWORKSHOPID%" "%ARMASVRPATH%\@EpochHive"
+:: copy battleye files to live
+xcopy /s/e/y/q/i "%ARMASVRPATH%\@EpochHive\sc\BattlEye" "%ARMASVRPATH%\SC\BattlEye"
+:: update mission files
+xcopy /s/e/y/q/i "%ARMASVRPATH%\@EpochHive\MPMissions" "%ARMASVRPATH%\MPMissions"
