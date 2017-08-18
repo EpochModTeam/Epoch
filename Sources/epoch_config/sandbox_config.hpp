aea096ce6887ded4d6fb1665d0692a99b113e896
vbawol <vbawol@veteranbastards.com>
2017-08-18 08:37:39 -0500

    add showHUD settings #822
    Thanks to @Ghostrider-DbD-

--- Sources/epoch_config/sandbox_config.hpp
+++ Sources/epoch_config/sandbox_config.hpp
@@ -52,6 +52,19 @@ wreckRemovalMaxTime = 360;
 
 disableRandomization[] = {"All"};
 
+showHUD[] =
+{
+	true, // Scripted HUD (same as showHUD command)
+	true, // Vehicle + soldier info
+	true, // Vehicle radar
+	true, // Vehicle compass
+	true, // Tank direction indicator
+	false, // Commanding menu
+	false, // Group Bar
+	true, // HUD Weapon Cursors
+	false // Squad Radar
+};
+
 #include "Configs\CfgServicePoint.hpp"
 #include "Configs\CfgItemSort.hpp"
 #include "Configs\CfgTraderMissions.hpp"
