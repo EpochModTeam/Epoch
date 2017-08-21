383d67c28c2a0761e37bc26a032885e1bcca0e47
vbawol <vbawol@veteranbastards.com>
2017-08-21 11:25:51 -0500

    Check to see if define true/false will fix bool conversion errors

--- Sources/epoch_config/sandbox_config.hpp
+++ Sources/epoch_config/sandbox_config.hpp
@@ -9,7 +9,8 @@
     Github:
     https://github.com/EpochModTeam/Epoch/tree/release/Sources/epoch_config/config.hpp
 */
-
+#define true 1
+#define false 0
 author = "Epoch Mod Team";
 class Header
 {
