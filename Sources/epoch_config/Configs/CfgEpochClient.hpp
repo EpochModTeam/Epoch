d75fbeb04675e35539c60ccdbaa789f29bc90b29
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2017-09-24 15:16:51 -0500

    add karma levels in array

--- Sources/epoch_config/Configs/CfgEpochClient.hpp
+++ Sources/epoch_config/Configs/CfgEpochClient.hpp
@@ -120,6 +120,7 @@ class CfgEpochClient
 	};
 	defineCommunityStats[]  = {
 		// EPOCH_total + varName, starting value, {min,max or custom values}
+		{"Karma",1500,{-50000,50000,{-5000,-15000,-30000},{5000,15000,30000}}, // min, max, {Work In Progress levels}
 		{"Murders",0,{}},
 		{"Deaths",0,{}},
 		{"Suicides",0,{0,99999},500}, // min, max, "Suicide King" status
