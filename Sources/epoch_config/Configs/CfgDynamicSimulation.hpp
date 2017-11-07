82041a33b2ad1b35305b2c37e4ac2fe69045edf5
vbawol <vbawol@veteranbastards.com>
2017-11-06 19:46:03 -0600

    add missing isMovingCoefValue CfgDynamicSimulation var

--- Sources/epoch_config/Configs/CfgDynamicSimulation.hpp
+++ Sources/epoch_config/Configs/CfgDynamicSimulation.hpp
@@ -1,7 +1,7 @@
 class CfgDynamicSimulation
 {
 	//If this is false none of the below settings matter
-	enableDynamicSimulationSystem = true; 
+	enableDynamicSimulationSystem = true;
 
 	//Individual toggles for testing
 	vehicleDynamicSimulationSystem = true; // type car, air, boat
@@ -9,6 +9,7 @@ class CfgDynamicSimulation
 	baseDynamicSimulationSystem = true; // plot pole and base pieces
 
 	// Distances and Coef
+	isMovingCoefValue = 2;
 	groupDynSimDistance = 1600;
 	vehicleDynSimDistance = 1600;
 	emptyVehicleDynSimDistance = 1600;
