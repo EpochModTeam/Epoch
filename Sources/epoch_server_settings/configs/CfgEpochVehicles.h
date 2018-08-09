5da8470f27a7bbb2185fcf6a312ac393d7730400
He-Man <Nullimeyer@yahoo.de>
2018-08-09 17:49:35 +0200

    Usage of custom skins
    Do not add this before Epoch 1.3 client files!

--- Sources/epoch_server_settings/configs/CfgEpochVehicles.h
+++ Sources/epoch_server_settings/configs/CfgEpochVehicles.h
@@ -16,7 +16,22 @@ class CfgEpochVehicles
 {
     class C_Hatchback_01_EPOCH {
         textureSelectionIndex[] = {0};
-		availableColors[] = {{ "\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE01_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE02_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE03_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE04_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE05_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE06_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE07_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE08_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE09_CO.paa" }};
+		availableColors[] = {
+			{
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE01_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE02_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE03_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE04_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE05_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE06_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE07_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE08_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_BASE09_CO.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Hatchback_01_ext_white_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Hatchback_01_ext_white_rusty.paa"	// increase change
+			}
+		};
     };
 	class C_Hatchback_01_EPOCH1: C_Hatchback_01_EPOCH{};
 	class C_Hatchback_01_EPOCH2: C_Hatchback_01_EPOCH{};
@@ -24,7 +39,20 @@ class CfgEpochVehicles
 	class C_Hatchback_01_EPOCH4: C_Hatchback_01_EPOCH{};
     class C_Offroad_01_EPOCH {
         textureSelectionIndex[] = {0,1};
-		availableColors[] = {{ "\A3\soft_F\Offroad_01\Data\Offroad_01_ext_co.paa","\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE01_CO.paa","\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE02_CO.paa","\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE03_CO.paa","\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE04_CO.paa","\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE05_CO.paa" }};
+		availableColors[] = {
+			{
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_co.paa",
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE01_CO.paa",
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE02_CO.paa",
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE03_CO.paa",
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE04_CO.paa",
+				"\A3\soft_F\Offroad_01\Data\Offroad_01_ext_BASE05_CO.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Offroad_01_ext_beige_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Offroad_01_ext_blue_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Offroad_01_ext_red_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Offroad_01_ext_white_rusty.paa"
+			}
+		};
     };
 	class C_Offroad_01_EPOCH1: C_Offroad_01_EPOCH{};
 	class C_Offroad_01_EPOCH2: C_Offroad_01_EPOCH{};
@@ -32,7 +60,15 @@ class CfgEpochVehicles
 	class C_Offroad_01_EPOCH4: C_Offroad_01_EPOCH{};
     class C_SUV_01_EPOCH {
         textureSelectionIndex[] = {0};
-		availableColors[] = {{ "\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_CO.paa","\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_02_CO.paa","\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_03_CO.paa","\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_04_CO.paa" }};
+		availableColors[] = {
+			{
+				"\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_CO.paa",
+				"\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_02_CO.paa",
+				"\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_03_CO.paa",
+				"\A3\Soft_F_Gamma\SUV_01\Data\SUV_01_ext_04_CO.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\SUV_01_ext_rusty.paa"
+			}
+		};
     };
 	class C_SUV_01_EPOCH1: C_SUV_01_EPOCH{};
 	class C_SUV_01_EPOCH2: C_SUV_01_EPOCH{};
@@ -40,7 +76,12 @@ class CfgEpochVehicles
 	class C_SUV_01_EPOCH4: C_SUV_01_EPOCH{};
     class C_Van_01_transport_EPOCH {
         textureSelectionIndex[] = {0};
-        availableColors[] = {{ "\A3\soft_f_gamma\van_01\Data\van_01_ext_CO.paa","\A3\soft_f_gamma\van_01\Data\van_01_ext_red_CO.paa" }};
+        availableColors[] = {
+			{
+				"\A3\soft_f_gamma\van_01\Data\van_01_ext_CO.paa",
+				"\A3\soft_f_gamma\van_01\Data\van_01_ext_red_CO.paa"
+			}
+		};
     };
 	class C_Van_01_transport_EPOCH1: C_Van_01_transport_EPOCH{};
 	class C_Van_01_transport_EPOCH2: C_Van_01_transport_EPOCH{};
@@ -48,7 +89,16 @@ class CfgEpochVehicles
 	class C_Van_01_transport_EPOCH4: C_Van_01_transport_EPOCH{};
     class C_Hatchback_02_EPOCH {
         textureSelectionIndex[] = {0};
-		availableColors[] = {{ "\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT01_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT02_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT03_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT04_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT05_CO.paa","\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT06_CO.paa" }};
+		availableColors[] = {
+			{
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT01_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT02_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT03_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT04_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT05_CO.paa",
+				"\A3\soft_f_gamma\Hatchback_01\data\Hatchback_01_ext_SPORT06_CO.paa"
+			}
+		};
     };
 	class C_Hatchback_02_EPOCH1: C_Hatchback_02_EPOCH{};
 	class C_Hatchback_02_EPOCH2: C_Hatchback_02_EPOCH{};
@@ -56,20 +106,97 @@ class CfgEpochVehicles
 	class C_Hatchback_02_EPOCH4: C_Hatchback_02_EPOCH{};
     class C_Quadbike_01_EPOCH {
         textureSelectionIndex[] = {0,1};
-		availableColors[] = {{ "\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_BLACK_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_BLUE_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_RED_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_WHITE_CO.paa" },{ "\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVBLACK_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVBLUE_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVRED_CO.paa","\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVWHITE_CO.paa" }};
+		availableColors[] = {
+			{
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_BLACK_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_BLUE_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_RED_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_CIV_WHITE_CO.paa"
+			},
+			{ 
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVBLACK_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVBLUE_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVRED_CO.paa",
+				"\A3\Soft_F_Beta\Quadbike_01\Data\Quadbike_01_wheel_CIVWHITE_CO.paa"
+			}
+		};
     };
     class C_Van_01_box_EPOCH {
         textureSelectionIndex[] = {0};
-        availableColors[] = {{ "\A3\soft_f_gamma\van_01\Data\van_01_ext_CO.paa","\A3\soft_f_gamma\van_01\Data\van_01_ext_red_CO.paa" }};
+        availableColors[] = {
+			{
+				"\A3\soft_f_gamma\van_01\Data\van_01_ext_CO.paa",
+				"\A3\soft_f_gamma\van_01\Data\van_01_ext_red_CO.paa"
+			}
+		};
     };
 	class C_Van_01_box_EPOCH1: C_Van_01_box_EPOCH{};
 	class C_Van_01_box_EPOCH2: C_Van_01_box_EPOCH{};
 	class C_Van_01_box_EPOCH3: C_Van_01_box_EPOCH{};
 	class C_Van_01_box_EPOCH4: C_Van_01_box_EPOCH{};
     class C_Heli_Light_01_civil_EPOCH {
-        textureSelectionIndex[] = { 0 };
-        availableColors[] = { { "A3\Air_f\Heli_Light_01\Data\Heli_Light_01_ext_co.paa", "\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_blue_co.paa", "\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_co.paa", "\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_ion_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_blueLine_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_digital_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_elliptical_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_furious_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_graywatcher_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_jeans_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_light_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_shadow_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sheriff_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_speedy_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sunset_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_vrana_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_wasp_co.paa", "\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_wave_co.paa" } };
+        textureSelectionIndex[] = {0};
+        availableColors[] = {
+			{
+				"A3\Air_f\Heli_Light_01\Data\Heli_Light_01_ext_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_blue_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\heli_light_01_ext_ion_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_blueLine_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_digital_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_elliptical_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_furious_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_graywatcher_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_jeans_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_light_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_shadow_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sheriff_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_speedy_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_sunset_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_vrana_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_wasp_co.paa",
+				"\a3\air_f\Heli_Light_01\Data\Skins\heli_light_01_ext_wave_co.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Heli_Light_01_ext_white_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Heli_Light_01_ext_white_rusty.paa",	// increase change
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\Heli_Light_01_ext_white_rusty.paa"	// increase change
+			}
+		};
     };
 	class C_Heli_Light_01_civil_2seat_EPOCH: C_Heli_Light_01_civil_EPOCH{};
 	class C_Heli_Light_01_civil_4seat_EPOCH: C_Heli_Light_01_civil_EPOCH{};
+    class C_Plane_Civil_01_F {
+        textureSelectionIndex[] = {0,1};
+        availableColors[] = {
+			{
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_RedLine_co.paa",
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Wave_co.paa",
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Tribal_co.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_01_RedLine_brownblack_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_01_Tribal_brownblack_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_01_white_browngreen_rusty.paa"
+			},
+			{
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_RedLine_co.paa",
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Wave_co.paa",
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Tribal_co.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_02_RedLine_brownblack_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_02_Tribal_brownblack_rusty.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_02_white_browngreen_rusty.paa"
+			}
+		};
+    };
+	
+    class C_Plane_Civil_01_racing_F {
+        textureSelectionIndex[] = {0,1};
+        availableColors[] = {
+			{
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_01_Racer_co.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_01_Racer_brownblack_rusty.paa"
+			},
+			{
+				"A3\Air_F_Exp\Plane_Civil_01\Data\btt_ext_02_Racer_co.paa",
+				"\x\addons\a3_epoch_community\textures\vehicle_customskins\btt_ext_02_Racer_brownblack_rusty.paa"
+			}
+		};
+    };
 };
